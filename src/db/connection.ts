d971199f205533dc33b41117e6505ff7c9a3f200
Ismenia Cristina <snowismscris@gmail.com>
2025-07-10 10:31:15 -0300

    configuração com o bd

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,7 +2,3 @@ import postgres from 'postgres'
 import { env } from '../env.ts'
 
 export const sql = postgres(env.DATABASE_URL)
-
-const result = await sql`SELECT 'Hello' as message`
-
-console.log(result)
